705f84268f776a82481249bbcce8bddca1824643
Marlon Couto <marloncouto.dev@gmail.com>
2022-11-30 21:22:36 -0300

    Adiciona mais dois botões ao App.js
    Signed-off-by: Marlon Couto <marloncouto.dev@gmail.com>

--- src/App.js
+++ src/App.js
@@ -4,8 +4,22 @@ function handleClick() {
   console.log("Clique");
 }
 
+function handleClick2() {
+  console.log("Outro clique");
+}
+
+function handleClick3() {
+  console.log("Mais um clique");
+}
+
 export default class App extends Component {
   render() {
-    return <button onClick={handleClick}>Clique-me</button>;
+    return (
+      <div>
+        <button onClick={handleClick}>Clique-me</button>
+        <button onClick={handleClick2}>Clique-me</button>
+        <button onClick={handleClick3}>Clique-me</button>
+      </div>
+    );
   }
 }
