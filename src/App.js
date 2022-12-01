f53b436935159f985ed43c6515146ae5d0c90888
Marlon Couto <marloncouto.dev@gmail.com>
2022-11-30 21:48:24 -0300

    Atualiza App.js redeclarando as funções dentro do componente
    Signed-off-by: Marlon Couto <marloncouto.dev@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React, { Component } from "react";
 
-function handleClick() {
-  console.log("Clique");
-}
-
-function handleClick2() {
-  console.log("Outro clique");
-}
-
-function handleClick3() {
-  console.log("Mais um clique");
-}
-
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClick2 = this.handleClick2.bind(this);
+    this.handleClick3 = this.handleClick3.bind(this);
+  }
+
+  handleClick() {
+    console.log("Clique");
+  }
+
+  handleClick2() {
+    console.log("Outro clique");
+  }
+
+  handleClick3() {
+    console.log("Mais um clique");
+  }
+
   render() {
     return (
       <div>
-        <button onClick={handleClick}>Clique-me</button>
-        <button onClick={handleClick2}>Clique-me</button>
-        <button onClick={handleClick3}>Clique-me</button>
+        <button onClick={this.handleClick}>Clique-me</button>
+        <button onClick={this.handleClick2}>Clique-me</button>
+        <button onClick={this.handleClick3}>Clique-me</button>
       </div>
     );
   }
